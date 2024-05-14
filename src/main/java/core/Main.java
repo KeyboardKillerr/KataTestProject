9638932b0f9407c0596ffd88275df6cfc1171d86
keyboardkillerr <radiation2033@gmail.com>
2024-05-14 14:41:09 +0300

    console input added

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -4,13 +4,16 @@ import core.calculations.exceptions.*;
 import core.calculations.parsers.exceptions.*;
 import core.calculations.*;
 import core.calculations.parsers.*;
-
-import java.util.Objects;
+import java.util.Scanner;
 
 public class Main {
     private static final String exceptionMessage = "throws Exception";
+    private static final Scanner scanner = new Scanner(System.in);
 
-    //public static void main(String[] args) { }
+    public static void main(String[] args) {
+        if (args != null && args.length != 0) System.out.println(calc(args[0]));
+        else System.out.println(calc(scanner.nextLine()));
+    }
     public static String calc(String input) {
         var intParser = new IntegerParser();
         var romanParser = new RomanParser();

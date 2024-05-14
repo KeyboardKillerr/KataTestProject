2f44a4dd7ae5ba60209c303639060c735bdb85ab
keyboardkillerr <radiation2033@gmail.com>
2024-05-14 14:35:33 +0300

    console input added

--- src/test/java/MainTests.java
+++ src/test/java/MainTests.java
@@ -1,7 +1,26 @@
 import core.Main;
 import org.junit.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
 public class MainTests {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+
     @Test
     public void sampleTests() {
         var result1 = Main.calc("1 + 2");
@@ -16,5 +35,24 @@ public class MainTests {
         Assert.assertEquals("throws Exception", result4);
         Assert.assertEquals("throws Exception", result5);
         Assert.assertEquals("throws Exception", result6);
+    }
+
+    @Test
+    public void mainWithArgsTest() {
+        String[] given = new String[] { "2+2" };
+        String expected = "4";
+        Main.main(given);
+        String result = outContent.toString().replace("\r\n", "");
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void mainWithConsoleInputTest() {
+        InputStream given = new ByteArrayInputStream("2+2".getBytes());
+        System.setIn(given);
+        String expected = "4";
+        Main.main(null);
+        String result = outContent.toString().replace("\r\n", "");
+        Assert.assertEquals(expected, result);
     }
 }
